421f7ebe4ab43d31277c94bb1aef7bb0b46b57f6
Marika <mballely@gmail.com>
2016-10-13 16:26:09 -0700

    begin fourth challenge

--- sum.js
+++ sum.js
@@ -9,6 +9,7 @@ var simpleAdder = function(arr) {
   return sum;
 }
 
+
 // Make a function that accepts two array as arguments and combines them into a single array
 
 var arraySmoosher = function(arr1, arr2) {
@@ -25,10 +26,10 @@ var arraySmoosher = function(arr1, arr2) {
   return newarr;
 }
 
+
 // Make a function that accepts an array as the first argument and another input as a second argument.  The function should remove all instances of the second argument from the inital array, returning a "cleaned" array.
 
 var censorship = function(arr, redact) {
-
     newarr = [];
 
     for (var i = 0; i < arr.length; i++) {
@@ -39,3 +40,17 @@ var censorship = function(arr, redact) {
 
     return newarr;
 }
+
+
+// Make a function that accepts an array as an argument and feed it an array of lowercased words.  The function should uppercase all words in the array that start with a vowel and return the modified array
+
+// var vowelsUp = function(lowercasedArray) {
+//
+//     for (var i = 0; i < lowercasedArray.length; i++) {
+//         if (lowercasedArray[i][0] === "a") {
+//             lowercasedArray[i][0] = "A";
+//         }
+//     }
+//
+//     return lowercasedArray;
+// }
